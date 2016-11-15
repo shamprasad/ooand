87dea6faab8a3f08f8d3096f7f1aec2719a42d8c
wadewu <wuqingjun@gmail.com>
2016-11-15 13:13:05 -0800

    move everything into eclipse projects.

---
+++ WeChatClient/src/WeChat/ClientWindow.java
@@ -0,0 +1,220 @@
+package WeChat;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+/*
+ * The Client with its GUI
+ */
+public class ClientWindow extends JFrame implements ActionListener {
+
+	// will first hold "Username:", later on "Enter message"
+	private JLabel label;
+	// to hold the Username and later on the messages
+	private JTextField textField;
+	// to hold the server address an the port number
+	private JTextField ServerAddress, ServerPort;
+	// to Logout and get the list of the users
+	private JButton login, logout, OnlineUsers;
+	// for the chat room
+	private JTextArea TextArea;
+	// if it is for connection
+	private boolean connected;
+	// the Client object
+	private Client client;
+	// the default port number
+	private int defaultPort;
+	private String defaultHost;
+
+	// Constructor connection receiving a socket number
+	ClientWindow(String host, int port) {
+
+		super("Chat Client");
+		defaultPort = port;
+		defaultHost = host;
+		
+		// The NorthPanel with:
+		JPanel northPanel = new JPanel(new GridLayout(3,1));
+		northPanel.setBackground(Color.BLACK);
+		// the server name anmd the port number
+		JPanel serverAndPort = new JPanel(new GridLayout(1,5, 1, 3));
+		// the two JTextField with default value for server address and port number
+		ServerAddress = new JTextField(host);
+		ServerPort = new JTextField("" + port);
+		ServerPort.setBackground(Color.BLACK);
+		ServerPort.setForeground(Color.WHITE);
+		ServerAddress.setBackground(Color.BLACK);
+		ServerAddress.setForeground(Color.WHITE);
+		serverAndPort.setBackground(Color.BLACK);
+		ServerPort.setHorizontalAlignment(SwingConstants.RIGHT);
+		JLabel jl = new JLabel("Server Address:  ");
+		jl.setForeground(Color.WHITE);
+		serverAndPort.add(jl);
+		serverAndPort.add(ServerAddress);
+		JLabel jl1 = new JLabel("Server Port:  ");
+		jl1.setForeground(Color.WHITE);
+		serverAndPort.add(jl1);
+		serverAndPort.add(ServerPort);
+		serverAndPort.add(new JLabel(""));
+		// adds the Server an port field to the GUI
+		northPanel.add(serverAndPort);
+
+		// the Label and the TextField
+		label = new JLabel("Enter your username below", SwingConstants.CENTER);
+		label.setForeground(Color.WHITE);
+		label.setBorder(new LineBorder(Color.WHITE));
+		northPanel.add(label);
+		textField = new JTextField("User Name: No authentication till now ");
+		textField.setBackground(Color.BLACK);
+		textField.setForeground(Color.WHITE);
+		northPanel.add(textField);
+		add(northPanel, BorderLayout.NORTH);
+
+		// The CenterPanel which is the chat room
+		TextArea = new JTextArea("Welcome\n", 80, 80);
+		TextArea.setBackground(Color.BLACK);
+		TextArea.setForeground(Color.WHITE);
+		//will be empty, but as soon 
+		//as user clicks on a user we need to display the most recent message
+		JPanel centerPanel = new JPanel(new GridLayout(1,1));
+		centerPanel.add(new JScrollPane(TextArea));
+		TextArea.setEditable(false);
+		add(centerPanel, BorderLayout.CENTER);
+
+		// the 3 buttons
+		login = new JButton("Login");
+		login.setBackground(Color.BLACK);
+		login.setForeground(Color.WHITE);
+		login.setBorder(new LineBorder(Color.WHITE));
+		login.addActionListener(this);
+		logout = new JButton("Logout");
+		logout.setBackground(Color.BLACK);
+		logout.setForeground(Color.WHITE);
+		logout.setBorder(new LineBorder(Color.WHITE));
+		logout.addActionListener(this);
+		logout.setEnabled(false);		// you have to login before being able to logout
+		OnlineUsers = new JButton("Who is in");//this should be used to display the online users, all right now
+		//needs to be restricted to the users contact list only
+		//backend not implemented yet
+		//this is going to take some time
+		OnlineUsers.addActionListener(this);
+		OnlineUsers.setEnabled(false);		// you have to login before being able to Who is in
+
+		JPanel southPanel = new JPanel();
+		southPanel.add(login);
+		southPanel.add(logout);
+		southPanel.add(OnlineUsers);
+		add(southPanel, BorderLayout.SOUTH);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(600, 600);
+		setVisible(true);
+		textField.requestFocus();
+
+	}
+
+	// called by the Client to append text in the TextArea 
+	void append(String str) {
+		TextArea.append(str);
+		TextArea.setCaretPosition(TextArea.getText().length() - 1);
+	}
+	// called by the GUI is the connection failed
+	// we reset our buttons, label, text field
+	void connectionFailed() {
+		login.setEnabled(true);
+		logout.setEnabled(false);
+		OnlineUsers.setEnabled(false);
+		label.setText("Enter your username below");
+		textField.setText("Anonymous");
+		// reset port number and host name as a construction time
+		ServerPort.setText("" + defaultPort);
+		ServerAddress.setText(defaultHost);
+		// let the user change them
+		ServerAddress.setEditable(false);
+		ServerPort.setEditable(false);
+		// don't react to a <CR> after the user name
+		textField.removeActionListener(this);
+		connected = false;
+	}
+		
+	/*
+	* Button or JTextField clicked
+	*/
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		// if it is the Logout button
+		if(o == logout) {
+			client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+			return;
+		}
+		// if it the who is in button
+		if(o == OnlineUsers) {
+			client.sendMessage(new ChatMessage(ChatMessage.OnlineUsers, ""));				
+			return;
+		}
+
+		// ok it is coming from the JTextField
+		if(connected) {
+			// just have to send the message
+			client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, textField.getText()));				
+			textField.setText("");
+			return;
+		}
+		
+
+		if(o == login) {
+			// ok it is a connection request
+			String username = textField.getText().trim();
+			// empty username ignore it
+			if(username.length() == 0)
+				return;
+			// empty serverAddress ignore it
+			String server = ServerAddress.getText().trim();
+			if(server.length() == 0)
+				return;
+			// empty or invalid port numer, ignore it
+			String portNumber = ServerPort.getText().trim();
+			if(portNumber.length() == 0)
+				return;
+			int port = 0;
+			try {
+				port = Integer.parseInt(portNumber);
+			}
+			catch(Exception en) {
+				return;   // nothing I can do if port number is not valid
+			}
+
+			// try creating a new Client with GUI
+			client = new Client(server, port, username, this);
+			// test if we can start the Client
+			if(!client.start()) 
+				return;
+			textField.setText("");
+			label.setText("Enter your message below");
+			connected = true;
+			
+			// disable login button
+			login.setEnabled(false);
+			// enable the 2 buttons
+			logout.setEnabled(true);
+			OnlineUsers.setEnabled(true);
+			// disable the Server and Port JTextField
+			ServerAddress.setEditable(false);
+			ServerPort.setEditable(false);
+			// Action listener for when the user enter a message
+			textField.addActionListener(this);
+		}
+
+	}
+
+	// to start the whole thing the server
+	public static void main(String[] args) {
+		new ClientWindow("localhost", 1500);
+	}
+
+}
+
---
+++ WeChatServer/src/wechat/ServerLog.java
@@ -0,0 +1,179 @@
+package wechat;
+
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * GUI for the server activity tracking.
+ * The message goes to the server and is then propagated to the receiver/group of receivers
+ * The logging takes place asynchronously into the db
+ * The  ServerLog class is a creator of the server class 
+ */
+public class ServerLog extends JFrame implements ActionListener, WindowListener {
+	
+	
+	// Stop and start buttons to shutdown/start the server
+	//Now this doesn't have to be that way.This is just for demo. Closing the gui sholdn't shut down the server.
+	private JButton serverStartStop;
+	
+	// Text Area for the chat log 
+	//text Area for events like logging in, connection request activity, etc
+	private JTextArea logSpace, eventSpace;
+	
+	// The server port number/right now hardcoded to 1500
+	private JTextField SPort;
+	
+	//Our instance of server can be generated. This allows us to create multiple servers.
+	private Server server;
+	
+	
+	// We are passing the hard coded port number 1500
+	//Calls the super constructor and creates new jframe with specified title
+	ServerLog(int port) {
+		super("Server Activity");
+		getContentPane().setBackground(Color.BLACK);
+		
+		// in the NorthPanel the PortNumber the Start and Stop buttons
+		JPanel top = new JPanel();
+		JLabel jl = new JLabel("Server Port Number: ");
+		jl.setForeground(Color.WHITE);
+		top.add(jl);
+		SPort = new JTextField("  " + port);
+		SPort.setForeground(Color.WHITE);
+		SPort.setBackground(Color.BLACK);
+		top.add(SPort);
+		top.setBackground(Color.BLACK);
+		
+		//init server
+		server = null;
+		//"Start" button
+		serverStartStop = new JButton("Start");
+		serverStartStop.addActionListener(this);
+		serverStartStop.setToolTipText("Start/Stop Server");
+		serverStartStop.setForeground(Color.WHITE);
+		serverStartStop.setBackground(Color.BLACK);
+		serverStartStop.setBorder(new LineBorder(Color.WHITE));
+		top.add(serverStartStop);
+		add(top, BorderLayout.NORTH);
+		
+		// the eventSpace and chatspace
+		JPanel center = new JPanel(new GridLayout(2,1));
+		logSpace = new JTextArea(80,80);
+		logSpace.setEditable(false);
+		center.setBackground(Color.BLACK);
+		logSpace.setForeground(Color.WHITE);
+		logSpace.setBackground(Color.BLACK);
+		appendRoom("Logs..... \n");
+		JScrollPane jSc1 = new JScrollPane(logSpace);
+		jSc1.setBackground(Color.BLACK);
+		jSc1.setBorder(new LineBorder(Color.WHITE));
+		center.add(jSc1);
+		eventSpace = new JTextArea(80,80);
+		eventSpace.setEditable(false);
+		eventSpace.setForeground(Color.WHITE);
+		eventSpace.setBackground(Color.BLACK);
+		appendEvent("Events.....\n");
+		JScrollPane jSc = new JScrollPane(eventSpace);
+		jSc.setBackground(Color.BLACK);
+		jSc.setBorder(new LineBorder(Color.WHITE));
+		center.add(jSc);	
+		add(center);
+		
+		// user clicks the close button on the frame
+		addWindowListener(this);
+		setSize(600, 600);
+		setVisible(true);
+	}		
+
+	// append message to the two JTextArea
+	// position at the end
+	void appendRoom(String str) {
+		logSpace.append(str);
+		logSpace.setCaretPosition(logSpace.getText().length() - 1);
+	}
+	void appendEvent(String str) {
+		eventSpace.append(str);
+		eventSpace.setCaretPosition(logSpace.getText().length() - 1);
+		
+	}
+	
+	// start or stop where clicked
+	public void actionPerformed(ActionEvent e) {
+		// if running we have to stop
+		if(server != null) {
+			server.stop();
+			server = null;
+			SPort.setEditable(true);
+			serverStartStop.setText("Start");
+			return;
+		}
+      	// OK start the server	
+		int port;
+		try {
+			port = Integer.parseInt(SPort.getText().trim());
+		}
+		catch(Exception er) {
+			appendEvent("Invalid port number");
+			return;
+		}
+		// ceate a new Server
+		server = new Server(port, this);
+		// and start it as a thread
+		new ServerRunning().start();
+		serverStartStop.setText("Stop");
+		SPort.setEditable(false);
+	}
+	
+	// entry point to start the Server
+	public static void main(String[] arg) {
+		// start server default port 1500
+		new ServerLog(1500);
+	}
+
+	/*
+	 * If the user click the X button to close the application
+	 * I need to close the connection with the server to free the port
+	 */
+	public void windowClosing(WindowEvent e) {
+		// if my Server exist
+		if(server != null) {
+			try {
+				server.stop();			// ask the server to close the conection
+			}
+			catch(Exception eClose) {
+			}
+			server = null;
+		}
+		// dispose the frame
+		dispose();
+		System.exit(0);
+	}
+	// I can ignore the other WindowListener method
+	public void windowClosed(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+
+	/*
+	 * A thread to run the Server
+	 */
+	class ServerRunning extends Thread {
+		public void run() {
+			server.start();         // should execute until if fails
+			// the server failed
+			serverStartStop.setText("Start");
+			SPort.setEditable(true);
+			appendEvent("Server crashed\n");
+			server = null;
+		}
+	}
+
+}
+
+
